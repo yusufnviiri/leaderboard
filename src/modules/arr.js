40565e38f39afe57cf17f78c9a431976dcaeca0b
yusufnviiri <yusufnviiri@gmail.com>
2022-05-20 19:16:56 +0300

    Implement GET and POST functions

--- src/modules/arr.js
+++ src/modules/arr.js
@@ -1,28 +1,12 @@
-export const scores = [
-  {
-    name: 'paul',
-    score: 80,
-  },
-  {
-    name: 'shafik',
-    score: 46,
-  },
-  {
-    name: 'Bogere',
-    score: 100,
-  },
-  {
-    name: 'Philip',
-    score: 88,
-  },
-  {
-    name: 'Yasin',
-    score: 98,
-  },
-];
-export const displayScores = (scores) => {
-  const marks = document.querySelector('.scores');
-  scores.forEach((score, index) => {
-    marks.innerHTML += `  <p class="${index % 2 !== 0 ? 'item' : 'item1'}">${score.name}:<span> ${score.score}</span></p>`;
+const scoredisplay = document.querySelector('.scores');
+
+export const displayScores = (allMembers) => {
+  const allUsers = (allMembers.result);
+  let items = '';
+  allUsers.forEach((allUsers, index) => {
+    items += `<p class="${index % 2 !== 0 ? 'item' : 'item1'}">${allUsers.user}:<span> ${allUsers.score}</span></p>`;
+    scoredisplay.innerHTML = items;
   });
 };
+
+export default displayScores;
